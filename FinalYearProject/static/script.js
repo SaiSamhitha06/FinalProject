2bfff27b274cae93395e8418da51a79978122783
SaiSamhitha06 <163017954+SaiSamhitha06@users.noreply.github.com>
2025-03-23 15:33:36 +0530

    Update script.js

--- FinalYearProject/static/script.js
+++ FinalYearProject/static/script.js
@@ -2,7 +2,7 @@ function sendMessage() {
     let userMessage = document.getElementById("userInput").value;
     if (!userMessage.trim()) return;
 
-    fetch("http://127.0.0.1:5000/chat", {
+    fetch("https://finalproject-6dtp.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userMessage })
